88a7ceff14ca1644bdabe7d7ed576213430c7fbd
Sam Varahram <sava3149@vitsippa.it.uu.se>
2025-03-05 16:47:38 +0100

    Not faster but its working

---
+++ A4/Pthreads/galsim.c
@@ -0,0 +1,265 @@
+// This version uses pthreads to parallelize the simulation loop.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+// Struct to store the position or velocity of a body
+typedef struct {
+    double x;
+    double y;
+} Vector2D;
+
+// Function prototypes for the serial functions (not used by the parallel code)
+// We now inline the work in the threads.
+static double get_wall_seconds();
+
+// Structure to pass thread arguments
+typedef struct {
+    int tid;           // thread id
+    int start;         // starting index (inclusive)
+    pthread_mutex_t *mutex;
+    int end;           // ending index (exclusive)
+    int nsteps;        // number of timesteps
+    int nstars;        // total number of stars
+    double stepsize;   // size of each timestep
+    double G;          // gravitational constant (scaled)
+    double e0;         // softening factor
+    Vector2D *position;
+    Vector2D *velocity;
+    double *mass;
+    double *ACCx;
+    double *ACCy;
+} ThreadData;
+
+// Global barrier for synchronization FIX
+pthread_barrier_t barrier;
+
+
+void *simulation_thread(void *arg) {
+    ThreadData *data = (ThreadData *) arg;
+    int nstars = data->nstars;
+    double stepsize = data->stepsize;
+    double G = data->G;
+    double e0 = data->e0;
+    
+    for (int t = 0; t < data->nsteps; t++) {
+        // --- Force Calculation ---
+        // For each star i in this partition, compute acceleration from all other stars.
+         // Reset acceleration arrays to zero
+    for (int i = data->start; i < data->end; i++) {
+        data->ACCx[i] = 0;
+        data->ACCy[i] = 0;
+    }
+    pthread_barrier_wait(&barrier);
+    for (int i = data->start; i < data->end; i++) {
+        double posx = data->position[i].x;
+        double posy = data->position[i].y;
+        double mass_i = data->mass[i];
+        double accx = 0;
+        double accy = 0;
+        for (int j = i+1; j < nstars; j++) {
+
+            double dx = posx - data->position[j].x;
+            double dy = posy - data->position[j].y;
+            double rije0 = sqrt(dx * dx + dy * dy) + e0;
+            double pow_rije0 = 1.0 / (rije0 * rije0 * rije0);
+            
+            double factor_i = data->mass[j] * pow_rije0;
+            double factor_j = mass_i * pow_rije0;
+            
+            accx -= factor_i * dx;
+            accy -= factor_i * dy;
+            pthread_mutex_lock(&data->mutex[j]);
+            data->ACCx[j] += factor_j * dx;
+            data->ACCy[j] += factor_j * dy;
+            pthread_mutex_unlock(&data->mutex[j]);
+        }
+        pthread_mutex_lock(&data->mutex[i]);
+        data->ACCx[i] += accx;
+        data->ACCy[i] += accy;
+        pthread_mutex_unlock(&data->mutex[i]);
+        }
+        // Synchronize to ensure all threads have computed forces
+        pthread_barrier_wait(&barrier);
+        // Now miltiply by G after all threads have computed forces
+        for (int i = data->start; i < data->end; i++) {
+            data->ACCx[i] *= G;
+            data->ACCy[i] *= G;
+        }
+
+
+        // Synchronize to ensure all threads have computed forces
+        pthread_barrier_wait(&barrier);
+        
+        // --- Update Velocities and Positions ---
+        for (int i = data->start; i < data->end; i++) {
+            data->velocity[i].x += stepsize * data->ACCx[i];
+            data->velocity[i].y += stepsize * data->ACCy[i];
+            data->position[i].x += stepsize * data->velocity[i].x;
+            data->position[i].y += stepsize * data->velocity[i].y;
+        }
+        // Synchronize to ensure all threads have finished updating before next timestep
+        pthread_barrier_wait(&barrier);
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    double start_time = get_wall_seconds();
+
+    if(argc != 7) {
+        fprintf(stderr, "Usage: %s <Number of stars> <input file> <Number of timesteps> <size of timesteps> <graphics> <Nthreads>\n", argv[0]);
+        return 1;
+    }
+
+    // Read command line arguments (make const for potential optimizations)
+    const int nstars = atoi(argv[1]);
+    const char* input_file = argv[2];
+    const int nsteps = atoi(argv[3]);
+    const double stepsize = atof(argv[4]);
+    const int graphics = atoi(argv[5]);  // 0 or 1 for false or true
+    int NUM_THREADS = atoi(argv[6]);
+    if (graphics) {
+        fprintf(stderr, "Graphics not supported\n");
+        return 1;
+    }
+
+    if (NUM_THREADS > nstars) {
+        NUM_THREADS = nstars;
+    }
+
+    const double G = 100.0 / nstars;
+    const double e0 = 1e-3; // softening factor
+
+    // Allocate arrays for simulation
+    int padding_size = nstars + 1;
+    Vector2D* position = (Vector2D*) malloc(padding_size * sizeof(Vector2D));
+    double* mass = (double*) malloc(nstars * sizeof(double));
+    Vector2D* velocity = (Vector2D*) malloc(padding_size * sizeof(Vector2D));
+    double* brightness = (double*) malloc(nstars * sizeof(double));
+    double* ACCx = (double*) malloc(padding_size * sizeof(double));
+    double* ACCy = (double*) malloc(padding_size * sizeof(double));
+
+    if (position == NULL || mass == NULL || velocity == NULL || brightness == NULL || ACCx == NULL || ACCy == NULL) {
+        fprintf(stderr, "Error allocating memory\n");
+        return 1;
+    }
+
+    // Initialize arrays to zero
+    memset(position, 0, nstars * sizeof(Vector2D));
+    memset(mass, 0, nstars * sizeof(double));
+    memset(velocity, 0, nstars * sizeof(Vector2D));
+    memset(brightness, 0, nstars * sizeof(double));
+    memset(ACCx, 0, nstars * sizeof(double));
+    memset(ACCy, 0, nstars * sizeof(double));
+
+    // Read input binary file
+    FILE* file = fopen(input_file, "rb");
+    if(file == NULL) {
+        fprintf(stderr, "Error opening file %s\n", input_file);
+        free(position);
+        free(mass);
+        free(velocity);
+        free(brightness);
+        free(ACCx);
+        free(ACCy);
+        return 1;
+    }
+    for (int i = 0; i < nstars; i++) {
+        if (fread(&position[i], sizeof(Vector2D), 1, file) != 1 ||
+            fread(&mass[i], sizeof(double), 1, file) != 1 ||
+            fread(&velocity[i], sizeof(Vector2D), 1, file) != 1 ||
+            fread(&brightness[i], sizeof(double), 1, file) != 1) {
+            fprintf(stderr, "Error reading file\n");
+            fclose(file);
+            return 1;
+        }
+    }
+    fclose(file);
+
+    // Determine number of threads to use and distribute work
+    pthread_t threads[NUM_THREADS];
+    ThreadData thread_data[NUM_THREADS];
+    int stars_per_thread = nstars / NUM_THREADS;
+    
+
+    pthread_mutex_t mutex[nstars];
+    
+    // Initialize barrier for NUM_THREADS threads
+    pthread_barrier_init(&barrier, NULL, NUM_THREADS);
+    // Initialize mutex for ACCx and ACCy
+    for (int i = 0; i < nstars; i++) {
+        pthread_mutex_init(&mutex[i], NULL);
+    }
+
+    // Create worker threads
+    int start = 0;
+    for (int t = 0; t < NUM_THREADS; t++) {
+        thread_data[t].tid = t;
+        thread_data[t].start = start;
+        thread_data[t].mutex = mutex;
+        // Distribute remainder stars to the last thread
+        thread_data[t].end = (t == NUM_THREADS-1 ? nstars : start + stars_per_thread);
+        thread_data[t].nstars = nstars;
+        thread_data[t].nsteps = nsteps;
+        thread_data[t].stepsize = stepsize;
+        thread_data[t].G = G;
+        thread_data[t].e0 = e0;
+        thread_data[t].position = position;
+        thread_data[t].velocity = velocity;
+        thread_data[t].mass = mass;
+        thread_data[t].ACCx = ACCx;
+        thread_data[t].ACCy = ACCy;
+        start += stars_per_thread;
+
+        if (pthread_create(&threads[t], NULL, simulation_thread, (void *) &thread_data[t]) != 0) {
+            fprintf(stderr, "Error creating thread %d\n", t);
+            exit(1);
+        }
+    }
+
+    // Wait for all threads to finish
+    for (int t = 0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
+    }
+    pthread_barrier_destroy(&barrier);
+    for (int i = 0; i < nstars; i++) {
+        pthread_mutex_destroy(&mutex[i]);
+    }
+
+    // Write out the results to a binary file
+    FILE* output = fopen("result.gal", "wb");
+    if(output == NULL) {
+        fprintf(stderr, "Error opening output file\n");
+        return 1;
+    }
+    for (int i = 0; i < nstars; i++) {
+        fwrite(&position[i], sizeof(Vector2D), 1, output);
+        fwrite(&mass[i], sizeof(double), 1, output);
+        fwrite(&velocity[i], sizeof(Vector2D), 1, output);
+        fwrite(&brightness[i], sizeof(double), 1, output);
+    }
+    fclose(output);
+
+    // Free allocated memory
+    free(ACCx);
+    free(ACCy);
+    free(position);
+    free(mass);
+    free(velocity);
+    free(brightness);
+
+    double end_time = get_wall_seconds();
+    printf("Time taken: %f seconds\n", end_time - start_time);
+    return 0;
+}
+
+static double get_wall_seconds() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + (double) tv.tv_usec / 1000000;
+}
